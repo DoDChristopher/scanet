d8bbabd0a073074f59b7e9bb24a94acf327c16fd
Sean David Lee <wvsi@aol.com>
2018-06-07 09:39:32 -0700

    pcl

--- Scanet/Scanet/MainPage.xaml.cs
+++ Scanet/Scanet/MainPage.xaml.cs
@@ -4,14 +4,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Scanet;
 
 namespace Scanet
 {
 	public partial class MainPage : ContentPage
 	{
-		public MainPage()
+        
+
+        public MainPage()
 		{
 			InitializeComponent();
+            
+            
 		}
-	}
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            title.ScaleTo(1.3, 2000, Easing.BounceIn);
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            
+        }
+
+        public async void OnEnter()
+        {
+            await title.ScaleTo(1, 2000, Easing.BounceOut);
+            
+            Scanner Scanner = new Scanner();
+            await Navigation.PushModalAsync(Scanner);
+        }
+
+    }
 }
